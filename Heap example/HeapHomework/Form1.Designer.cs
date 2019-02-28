eb42a4c3ae8604b13e290a1e1c8899bf86d47a12
PatrickTopor <35204198+PatrickTopor@users.noreply.github.com>
2019-02-28 08:22:06 -0800

    Add files via upload

---
+++ Heap example/HeapHomework/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace HeapHomework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ResNum = new System.Windows.Forms.TextBox();
+            this.ResFirst = new System.Windows.Forms.TextBox();
+            this.ResLast = new System.Windows.Forms.TextBox();
+            this.textbox3 = new System.Windows.Forms.TextBox();
+            this.textbox2 = new System.Windows.Forms.TextBox();
+            this.textbox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.buttonAddReser = new System.Windows.Forms.Button();
+            this.buttonGetReser = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ResNum
+            // 
+            this.ResNum.AccessibleName = "ResNum";
+            this.ResNum.Location = new System.Drawing.Point(224, 102);
+            this.ResNum.Margin = new System.Windows.Forms.Padding(6);
+            this.ResNum.Name = "ResNum";
+            this.ResNum.Size = new System.Drawing.Size(224, 31);
+            this.ResNum.TabIndex = 0;
+            // 
+            // ResFirst
+            // 
+            this.ResFirst.AccessibleName = "ResFirst";
+            this.ResFirst.Location = new System.Drawing.Point(224, 173);
+            this.ResFirst.Margin = new System.Windows.Forms.Padding(6);
+            this.ResFirst.Name = "ResFirst";
+            this.ResFirst.Size = new System.Drawing.Size(224, 31);
+            this.ResFirst.TabIndex = 1;
+            // 
+            // ResLast
+            // 
+            this.ResLast.AccessibleName = "ResLast";
+            this.ResLast.Location = new System.Drawing.Point(224, 252);
+            this.ResLast.Margin = new System.Windows.Forms.Padding(6);
+            this.ResLast.Name = "ResLast";
+            this.ResLast.Size = new System.Drawing.Size(224, 31);
+            this.ResLast.TabIndex = 2;
+            // 
+            // textbox3
+            // 
+            this.textbox3.AccessibleName = "textbox3";
+            this.textbox3.Location = new System.Drawing.Point(960, 669);
+            this.textbox3.Margin = new System.Windows.Forms.Padding(6);
+            this.textbox3.Name = "textbox3";
+            this.textbox3.Size = new System.Drawing.Size(290, 31);
+            this.textbox3.TabIndex = 5;
+            // 
+            // textbox2
+            // 
+            this.textbox2.AccessibleName = "textbox2";
+            this.textbox2.Location = new System.Drawing.Point(960, 590);
+            this.textbox2.Margin = new System.Windows.Forms.Padding(6);
+            this.textbox2.Name = "textbox2";
+            this.textbox2.Size = new System.Drawing.Size(290, 31);
+            this.textbox2.TabIndex = 4;
+            // 
+            // textbox1
+            // 
+            this.textbox1.AccessibleName = "textbox1";
+            this.textbox1.Location = new System.Drawing.Point(960, 519);
+            this.textbox1.Margin = new System.Windows.Forms.Padding(6);
+            this.textbox1.Name = "textbox1";
+            this.textbox1.Size = new System.Drawing.Size(290, 31);
+            this.textbox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 115);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Reservation Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 187);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(96, 265);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(812, 683);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Last Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(814, 604);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "First Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(720, 533);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(208, 25);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Reservation Number";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(218, 38);
+            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(232, 25);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Enter New Reservation";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(954, 402);
+            this.label8.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(217, 25);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Get Next Reservation";
+            // 
+            // buttonAddReser
+            // 
+            this.buttonAddReser.Location = new System.Drawing.Point(170, 323);
+            this.buttonAddReser.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonAddReser.Name = "buttonAddReser";
+            this.buttonAddReser.Size = new System.Drawing.Size(282, 44);
+            this.buttonAddReser.TabIndex = 14;
+            this.buttonAddReser.Text = "Enter New Reservation";
+            this.buttonAddReser.UseVisualStyleBackColor = true;
+            this.buttonAddReser.Click += new System.EventHandler(this.buttonAddReser_Click);
+            // 
+            // buttonGetReser
+            // 
+            this.buttonGetReser.Location = new System.Drawing.Point(876, 463);
+            this.buttonGetReser.Margin = new System.Windows.Forms.Padding(6);
+            this.buttonGetReser.Name = "buttonGetReser";
+            this.buttonGetReser.Size = new System.Drawing.Size(378, 44);
+            this.buttonGetReser.TabIndex = 15;
+            this.buttonGetReser.Text = "Fetch Highest Priority Reservation";
+            this.buttonGetReser.UseVisualStyleBackColor = true;
+            this.buttonGetReser.Click += new System.EventHandler(this.buttonGetReser_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1346, 850);
+            this.Controls.Add(this.buttonGetReser);
+            this.Controls.Add(this.buttonAddReser);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textbox3);
+            this.Controls.Add(this.textbox2);
+            this.Controls.Add(this.textbox1);
+            this.Controls.Add(this.ResLast);
+            this.Controls.Add(this.ResFirst);
+            this.Controls.Add(this.ResNum);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ResNum;
+        private System.Windows.Forms.TextBox ResFirst;
+        private System.Windows.Forms.TextBox ResLast;
+        private System.Windows.Forms.TextBox textbox3;
+        private System.Windows.Forms.TextBox textbox2;
+        private System.Windows.Forms.TextBox textbox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button buttonAddReser;
+        private System.Windows.Forms.Button buttonGetReser;
+    }
+}
+
---
+++ Heap example/HeapHomework/Form1.cs
@@ -0,0 +1,67 @@
+﻿using Prog260_Heap;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeapHomework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Heap myHeap;
+        Person person;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            myHeap = new Heap(20);
+        }
+
+        private void buttonAddReser_Click(object sender, EventArgs e)
+        {
+            //add reservation
+            int resNum = Convert.ToInt32(ResNum.Text);
+            string fName = ResFirst.Text;
+            string lName = ResLast.Text;
+            person = new Person(resNum, fName, lName);
+            myHeap.Insert(person);
+
+            //clear textboxes
+            ResNum.Clear();
+            ResFirst.Clear();
+            ResLast.Clear();
+
+        }
+
+        private void buttonGetReser_Click(object sender, EventArgs e)
+        {
+            //inform user heap is empty
+            if (myHeap.IsEmpty)
+            {
+                textbox1.Text = "No more reservations here";
+                textbox2.Clear();
+                textbox3.Clear();
+            }
+
+            //tell user the highest priority customer
+            else
+            {
+                person = myHeap.RemoveMaxNode();
+
+                textbox1.Text = Convert.ToString(person.ReservationNumber);
+                textbox2.Text = person.FirstName;
+                textbox3.Text = person.LastName;
+            }
+
+        }
+
+    }
+}
