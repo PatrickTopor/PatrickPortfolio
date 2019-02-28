478617a7b2b64ab5fd44ab46972320a7a50eb14e
PatrickTopor <35204198+PatrickTopor@users.noreply.github.com>
2019-02-28 08:10:30 -0800

    Add files via upload

---
+++ PatrickLinkedList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PatrickLinkedList
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			IntLinkedList MyLinkedList = new IntLinkedList();
+
+			MyLinkedList.InsertAtFront(101);
+
+			MyLinkedList.InsertAtFront(102);
+
+			MyLinkedList.InsertAtFront(103);
+
+			MyLinkedList.InsertAtFront(104);
+
+			MyLinkedList.InsertAtFront(105);
+
+			Console.WriteLine("should be 105  " + MyLinkedList.RemoveByValue(105));
+
+			Console.WriteLine("should be 101  " + MyLinkedList.RemoveByValue(101));
+
+			Console.WriteLine("should be 103  " + MyLinkedList.RemoveByValue(103));
+
+			MyLinkedList.Print();
+
+			Console.ReadLine();
+
+
+		}
+    }
+}
